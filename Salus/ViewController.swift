5b263e7c0831cef83b0a0a3a92bce53a25106889
Amol Kumar <amolkumar@amols-macbook-pro.local>
2016-07-16 15:58:47 +0530

    Initial Commit

---
+++ Salus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Salus
+//
+//  Created by Amol Kumar on 16/07/16.
+//  Copyright © 2016 Amol Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
